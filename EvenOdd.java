46c8fd4cfb23b26bd113a0edeb65b01ccb18cd95
Salony DHiman <salonydhiman292@gmail.com>
2025-11-17 09:48:57 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,20 @@
+// import java.util.Scanner;
+
+// public class EvenOdd {
+//     public static void main(String[] args) {
+//         //Scanner sc =new Scanner(System.in);
+//         // for printing
+//         System.out.println("enter a number");
+//         //int num=sc.nextInt();
+//         //int n=2;
+//         if
+//            //( num%2==0)
+//             {
+//                 System.out.println("num is even");
+//             }
+//             else{
+//                 System.out.println("no. is odd");
+//             }
+        
+//     }
+// }
