b4d504c62dbd5915bc2088357aa28d2b4bc14284
Salony DHiman <salonydhiman292@gmail.com>
2025-12-04 11:23:09 +0530

    Add files via upload

---
+++ helloworldForloop.java
@@ -0,0 +1,15 @@
+package loops;
+
+import java.util.Scanner;
+
+public class helloworldForloop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your number");
+        int n=sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            System.out.println("hello world");
+        }
+    }
+}
