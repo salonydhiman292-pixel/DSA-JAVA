795151b69acb52b23b2d40369de0996e0d1b1fdb
Salony DHiman <salonydhiman292@gmail.com>
2025-12-04 11:24:52 +0530

    Add files via upload

---
+++ print1to100.java
@@ -0,0 +1,9 @@
+package loops;
+
+public class print1to100 {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
