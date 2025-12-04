08f1cca0eb4e0d3d7290962ed564552edaa0efb0
Salony DHiman <salonydhiman292@gmail.com>
2025-12-04 13:12:24 +0530

    Add files via upload

---
+++ countDigit.java
@@ -0,0 +1,18 @@
+package loops;
+
+import java.util.Scanner;
+
+public class countDigit {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your number");
+        int n=sc.nextInt();
+
+       int count=0;
+       while(n!=0){
+           n/=10;
+           count++;
+       }
+        System.out.println(count);
+    }
+}
