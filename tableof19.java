ed930c20095f1dc200d83bbec83b722193fb9701
Salony DHiman <salonydhiman292@gmail.com>
2025-12-06 20:50:04 +0530

    Add files via upload

---
+++ tableof19.java
@@ -0,0 +1,9 @@
+package loops;
+
+public class tableof19 {
+    public static void main(String[] args) {
+        for(int i=19;i<=19*10;i=i+19){
+            System.out.println(i);
+        }
+    }
+}
