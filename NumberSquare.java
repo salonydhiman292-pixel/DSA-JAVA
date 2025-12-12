d9f35cbd8b01924cf99375772e1d109b04624af8
Salony DHiman <salonydhiman292@gmail.com>
2025-12-12 17:55:26 +0530

    Add files via upload

---
+++ NumberSquare.java
@@ -0,0 +1,22 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class NumberSquare {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter row");
+        int m=sc.nextInt();
+        System.out.println("enter column");
+        int n=sc.nextInt();
+
+        for (int i = 1; i <=m ; i++) {
+            for (int j = 1; j <=n ; j++) {
+                System.out.print(j);
+
+            }
+            System.out.println();
+
+        }
+    }
+}
