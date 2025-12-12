da90e5972ed8495dcee41f6927e9ee50759bf9fb
Salony DHiman <salonydhiman292@gmail.com>
2025-12-12 17:53:15 +0530

    Add files via upload

---
+++ alphabetSquare.java
@@ -0,0 +1,24 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class alphabetSquare {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter row");
+        int m = sc.nextInt();
+        System.out.println("enter column");
+        int n = sc.nextInt();
+
+        for (int i = 1 ;i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print((char)(j+64));
+
+            }
+            System.out.println();
+
+        }
+    }
+}
