6232c6b1d4584539964ca2cd25568a60c0771f99
Salony DHiman <salonydhiman292@gmail.com>
2025-12-12 17:59:22 +0530

    Add files via upload

---
+++ starTriangleFlipped.java
@@ -0,0 +1,25 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class starTriangleFlipped {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter row");
+        int m = sc.nextInt();
+//        System.out.println("enter column");
+//        int n = sc.nextInt();
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= m; j++) {
+                if ((i + j) > m)
+                    System.out.print("*");
+                else System.out.print(" ");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
