c1854cedcf7c824bd553bd016748ab23d708cd02
Salony DHiman <salonydhiman292@gmail.com>
2025-12-12 17:54:14 +0530

    Add files via upload

---
+++ alternatingstarnumberTriangle.java
@@ -0,0 +1,28 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class alternatingstarnumberTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter row");
+        int m = sc.nextInt();
+//        System.out.println("enter column");
+//        int n = sc.nextInt();
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i % 2 == 0) {
+                    System.out.print("1");
+//                    System.out.println();
+                } else {
+                    System.out.print("*");
+//                    System.out.print();
+                }
+//                System.out.println();
+
+            }
+            System.out.println();
+        }
+    }
+}
