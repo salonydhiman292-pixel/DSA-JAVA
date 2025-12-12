6e7311e71f309bb21c0fa8f6085ae67f86a6f93e
Salony DHiman <salonydhiman292@gmail.com>
2025-12-12 17:57:39 +0530

    Add files via upload

---
+++ starPyramid.java
@@ -0,0 +1,26 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class starPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter row");
+        int m = sc.nextInt();
+//        System.out.println("enter column");
+//        int n = sc.nextInt();
+
+        for (int i = 1 ;i <= m; i++) {
+            for (int j = 1; j <= m+1-i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
