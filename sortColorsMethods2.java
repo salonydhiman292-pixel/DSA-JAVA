5349d2dd9b39caef96e24ce7d53b4c5392c59602
Salony DHiman <salonydhiman292@gmail.com>
2025-12-17 17:12:34 +0530

    Add files via upload

---
+++ sortColorsMethods2.java
@@ -0,0 +1,39 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sortColorsMethods2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i = 0; i <n ; i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        int mid=0;
+        int low=0;
+        int high=n-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp=arr[mid];
+                arr[mid]=arr[low];
+                arr[low]=temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=temp;
+                high--;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
