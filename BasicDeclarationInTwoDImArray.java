f76202c02d2bd7f1abc76ecfdec20407edbb107d
Salony DHiman <salonydhiman292@gmail.com>
2026-01-14 12:02:09 +0530

    Add files via upload

---
+++ BasicDeclarationInTwoDImArray.java
@@ -0,0 +1,12 @@
+package TwoDimensionalArray;
+
+public class BasicDeclarationInTwoDImArray {
+    public static void main(String[] args) {
+        int[][] grid= new int[3][3];
+        grid[0][0]=10;
+        grid[0][1]=20;
+        grid[0][2]=30;
+        System.out.println(grid[2][2]);
+        System.out.println(grid[0][2]);
+    }
+}
