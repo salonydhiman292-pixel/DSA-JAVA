5823a7e50bb9e1273f6fd3b452ae5da2918032b4
Salony DHiman <salonydhiman292@gmail.com>
2026-01-15 21:36:03 +0530

    Add files via upload

---
+++ largestElementin2Darray.java
@@ -0,0 +1,28 @@
+package TwoDimensionalArray;
+
+import java.util.Scanner;
+
+public class largestElementin2Darray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[][] arr=new int[3][3];
+        for (int i = 0; i <3 ; i++) {
+            for (int j = 0; j <3 ; j++) {
+                arr[i][j]=sc.nextInt();
+
+            }
+
+        }
+        int max=Integer.MIN_VALUE;
+
+        for (int i = 0; i <3 ; i++) {
+            for (int j = 0; j <3 ; j++) {
+                if(max<=arr[i][j]){
+                    max=arr[i][j];
+                }
+            }
+
+        }
+        System.out.println(max);
+    }
+}
