40c2675b78d8e1c68803582d74a97b97219f68ca
Salony DHiman <salonydhiman292@gmail.com>
2026-02-22 16:04:00 +0530

    Add files via upload

---
+++ arrays/SpiralMatrix.java
@@ -0,0 +1,34 @@
+package array2;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25},{26,27,28,29,30}};
+        int m=arr.length;
+        int n=arr[0].length;
+        int minr=0;
+        int maxr=m-1;
+        int minc=0;
+        int maxc=n-1;
+    while(minr<=maxr && minc<=maxc){
+        for (int j = minc; j <=maxc ; j++) {
+            System.out.print(arr[minr][j]+" ");
+
+        }minr++;
+        if(minr>maxr || minc>maxc ) break;
+        for (int i = minr; i <=maxr ; i++) {
+            System.out.print(arr[i][maxc]+" ");
+
+        }maxc--;
+        if(minr>maxr || minc>maxc ) break;
+        for (int j = maxc; j >=minc ; j--) {
+            System.out.print(arr[maxr][j]+" ");
+
+        }
+    maxr--;
+        for (int i = maxr; i >=minr ; i--) {
+            System.out.print(arr[i][minc]+" ");
+
+        }
+    minc++;}
+    }
+}
